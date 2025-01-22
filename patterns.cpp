804b2f677263d95acd5e060e517f1ddc6542c713
Vivek Kumar <vivek.kumar@lpu.in>
2025-01-22 11:41:35 +0530

    Patterns concepts are added

--- patterns.cpp
+++ patterns.cpp
@@ -101,20 +101,34 @@ int main(){
 
     
     
-    char letter='Z';
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<i;j++){
-            cout<<" ";
-        }
+    // char letter='Z';
+    // for(int i=1;i<=5;i++){
+    //     for(int j=1;j<i;j++){
+    //         cout<<" ";
+    //     }
         
         
-        for (int j = 5; j >=i; j--) {
+    //     for (int j = 5; j >=i; j--) {
            
-            cout << (char)(letter)<<" ";
-             letter--;
+    //         cout << (char)(letter)<<" ";
+    //          letter--;
+    //     }
+    //     cout<<endl;
+    // }
+
+    for (int i = 0; i < 4; i++) {             
+        for (int j = 0; j < 6; j++) {         
+            if (i == 0 || i == 4 - 1) {       
+                cout << "*";
+            } else if (j == 0 || j == 6 - 1) {
+                cout << "*";
+            } else {      
+                cout << " ";
+            }
         }
-        cout<<endl;
+        cout << endl;                         
     }
+
     return 0;
 }
 
---
+++ pointers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int a;
+    // int *pa;
+    // pa = &a;
+    // a = 4;
+
+    // cout<<a<<endl;
+    // cout<<&a<<endl;
+    // cout<<pa<<endl;
+    // cout<<*pa<<endl;
+    // cout<<&pa<<endl;
+
+    int x = 10;
+    int y = 20;
+    int *px = &x;
+    int *py = &y;
+
+    cout<<x + y<<endl;
+    cout<<*px + *py <<endl;
+
+    
+    
+
+}
